4aa820aa2bce5f07101f20c585d83ca352546485
Matt Erhart <merhart@domaintools.com>
2017-12-06 14:34:08 -0800

    updates and stuff

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ function goleft(){
 
 function toggleForms()
 {
+    document.getElementsByClassName('modal')[0].style.display="flex";
     var form = document.getElementsByTagName('select')[0];
     switch(form.value)
     {
@@ -48,33 +49,36 @@ function toggleForms()
         break;
     }
 }
+function closeModal(){
+    document.getElementsByClassName('modal')[0].style.display="none";
+}
 var forms = ['<form style="display:block;width:80%;">\
-                <label for="names">Your Names:</label><input type="text" id="names" /><br>\
-                <label for="contact">Email:</label><input type="text" id="contact" /><br>\
-                <label for="dates">Wedding Date:</label><input type="text" id="dates" /><br>\
-                <label for="location">Wedding Location:</label><input type="text" id="location" /><br>\
-                <label class="long" for="ramble">Tell me about your love. Ramble to your heart\'s content:</label><textarea rows="6" id="ramble"></textarea>\
-                <label class="long" for="daydream">When day dreaming about your wedding day, what vibe do you envision:</label><textarea rows="6" id="daydream"></textarea>\
-                <label class="long" for="songs">Name your 3 most recently played songs:</label><textarea rows="6" id="songs"></textarea> <br>\
+                <label for="names">Your Names:</label><input type="text" id="names" />\
+                <label for="contact">Email:</label><input type="text" id="contact" />\
+                <label for="dates">Wedding Date:</label><input type="text" id="dates" />\
+                <label for="location">Wedding Location:</label><input type="text" id="location" />\
+                <label  for="ramble">Tell me about your love. Ramble to your heart\'s content:</label><textarea rows="6" id="ramble"></textarea>\
+                <label  for="daydream">When day dreaming about your wedding day, what vibe do you envision:</label><textarea rows="6" id="daydream"></textarea>\
+                <label  for="songs">Name your 3 most recently played songs:</label><textarea rows="6" id="songs"></textarea> \
               <div class = "filler" style="text-align: center;">\
               <a class="btn">submit</a></form>',
               '<form style="display:block;width:80%;">\
-                <label for="names">Your Name:</label><input type="text" id="names" /><br>\
-                <label for="contact">Email:</label><input type="text" id="contact" /><br>\
-                <label class="long" for="ramble">What story would you like Sugar Dream Studio to help you tell:</label><textarea rows="6" id="ramble"></textarea>\
-                <label class="long" for="daydream">What gets you out of bed in the morning?</label><textarea rows="6" id="daydream"></textarea>\
-                <label class="long" for="songs">Team Pizza. Team Hamburger. Or Team Burrito. Pick one:</label><textarea rows="6" id="songs"></textarea> <br>\
+                <label for="names">Your Name:</label><input type="text" id="names" />\
+                <label for="contact">Email:</label><input type="text" id="contact" />\
+                <label  for="ramble">What story would you like Sugar Dream Studio to help you tell:</label><textarea rows="6" id="ramble"></textarea>\
+                <label  for="daydream">What gets you out of bed in the morning?</label><textarea rows="6" id="daydream"></textarea>\
+                <label  for="songs">Team Pizza. Team Hamburger. Or Team Burrito. Pick one:</label><textarea rows="6" id="songs"></textarea> \
               <div class = "filler" style="text-align: center;">\
               <a class="btn">submit</a></form>',
               '<form style="display:block;width:80%;">\
-                <label for="names">Your Name:</label><input type="text" id="names" /><br>\
-                <label for="contact">Email:</label><input type="text" id="contact" /><br>\
-                <label class="long" for="desc">Event Name & Description:</label><textarea rows="6" id="desc"></textarea><br>\
-                <label for="dates">Event Date:</label><input type="text" id="dates" /><br>\
-                <label for="location">Event Location:</label><input type="text" id="location" /><br>\
-                <label class="long" for="ramble">What about Sugar Dream studio has piqued your interest:</label><textarea rows="6" id="ramble"></textarea>\
-                <label class="long" for="daydream">When day dreaming about your event, what vibe do you envision? </label><textarea rows="6" id="daydream"></textarea><br>\
+                <label for="names">Your Name:</label><input type="text" id="names" />\
+                <label for="contact">Email:</label><input type="text" id="contact" />\
+                <label  for="desc">Event Name & Description:</label><textarea rows="6" id="desc"></textarea>\
+                <label for="dates">Event Date:</label><input type="text" id="dates" />\
+                <label for="location">Event Location:</label><input type="text" id="location" />\
+                <label  for="ramble">What about Sugar Dream studio has piqued your interest:</label><textarea rows="6" id="ramble"></textarea>\
+                <label  for="daydream">When day dreaming about your event, what vibe do you envision? </label><textarea rows="6" id="daydream"></textarea>\
               <div class = "filler" style="text-align: center;">\
               <a class="btn">submit</a></form>',
-              '<p>Hey, ya know what? Just email me at <a style="color:white;text-decoration:none;font-weight: bold;" href="mailto:hello@sugardream.studio">hello@sugardream.studio</a> :)<br><br>I look forward to chatting with you!</p>'
+              '<p>Hey, ya know what? Just email me at <a style="color:white;text-decoration:none;font-weight: bold;" href="mailto:hello@sugardream.studio">hello@sugardream.studio</a> :)I look forward to chatting with you!</p>'
               ];
