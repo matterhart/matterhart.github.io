840420dd7f7d8d250c536061623d747c0d6c0d63
Matt Erhart <merhart@domaintools.com>
2017-12-09 19:01:03 -0800

    play updates

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@ function sendRequest(payload){
     xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
     xmlHttp.onload = function() {
-        document.getElementById('form-filler').innerHTML ='<p style="color:#777;margin:30px auto; width:75%;">Reac hout if you need anything, <a style="color:#777;text-decoration:none;font-weight: bold;" href="mailto:hello@sugardream.studio">hello@sugardream.studio</a> :) I look forward to chatting with you!</p>';
+        document.getElementById('form-filler').innerHTML ='<p style="color:#777;margin:30px auto; width:75%;">Reach out if you need anything, <a style="color:#777;text-decoration:none;font-weight: bold;" href="mailto:hello@sugardream.studio">hello@sugardream.studio</a> :) I look forward to chatting with you!</p>';
     }
   var params = 'payload='+encodeURIComponent(payload)+'&';
     xmlHttp.send( params);
